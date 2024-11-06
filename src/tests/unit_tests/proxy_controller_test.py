3dc43cfef1bf86955c47d59b1ba16fbb8aa49cef
yoori <yuri.kuznecov@gmail.com>
2024-11-06 11:04:25 +0000

    Fix ProxyController unit test

--- src/tests/unit_tests/proxy_controller_test.py
+++ src/tests/unit_tests/proxy_controller_test.py
@@ -1,18 +1,22 @@
 from flare_bypasser import ProxyController
 
-proxy_controller = ProxyController(command = 'echo "Listening on {{LOCAL_PORT}}"')
+def test_two_different_proxies_rent():
+  proxy_controller = ProxyController(command = 'echo "Listening on {{LOCAL_PORT}}"')
 
-with proxy_controller.get_proxy("socks5://127.0.0.1:7777") as proxy1, \
-  proxy_controller.get_proxy("socks5://127.0.0.1:7777") as proxy2 : # noqa
-  assert proxy_controller.opened_proxies_count() == 1
-  assert proxy1.local_port() == proxy2.local_port()
+  with proxy_controller.get_proxy("socks5://127.0.0.1:7777") as proxy1, \
+    proxy_controller.get_proxy("socks5://127.0.0.1:7777") as proxy2 : # noqa
+    assert proxy_controller.opened_proxies_count() == 1
+    assert proxy1.local_port() == proxy2.local_port()
 
-assert proxy_controller.opened_proxies_count() == 0
+  assert proxy_controller.opened_proxies_count() == 0
 
-with proxy_controller.get_proxy("socks5://127.0.0.1:7777") as proxy1, \
-  proxy_controller.get_proxy("socks5://127.0.0.1:7777") as proxy2 : # noqa
-  assert proxy_controller.opened_proxies_count() == 1
-  assert proxy1.local_port() == proxy2.local_port()
-  assert proxy1.is_alive()
+def test_two_equal_proxies_rent():
+  proxy_controller = ProxyController(command = 'echo "Listening on {{LOCAL_PORT}}"')
 
-assert proxy_controller.opened_proxies_count() == 0
+  with proxy_controller.get_proxy("socks5://127.0.0.1:7777") as proxy1, \
+    proxy_controller.get_proxy("socks5://127.0.0.1:7777") as proxy2 : # noqa
+    assert proxy_controller.opened_proxies_count() == 1
+    assert proxy1.local_port() == proxy2.local_port()
+    assert proxy1.is_alive()
+
+  assert proxy_controller.opened_proxies_count() == 0
